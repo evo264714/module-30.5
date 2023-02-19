42760076e218563ba1a2274809484b4d60f0add4
evo264714 <mh264714@gmail.com>
2023-02-19 17:23:44 +0600

    first

---
+++ recap.js
@@ -0,0 +1,14 @@
+/*
+* 1. How to use js with HTML and CSS
+* 2. Get element or elements from dom
+!      2-1: getElementById
+!      2-2: getElementsByClassName
+!      2-3: querySelector
+!      2-4: querySelectorAll  
+*
+*
+*
+*
+*
+
+*/

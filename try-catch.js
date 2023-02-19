cd54cd7a0d2b2d6b44e07f856a61017af4289bca
evo264714 <mh264714@gmail.com>
2023-02-19 20:01:23 +0600

    done

--- try-catch.js
+++ try-catch.js
@@ -1,3 +1,22 @@
 const day = 'friday';
 console.log(day);
 console.log(25);
+// console.log(district);
+// let district = 'bBaria';
+console.log(61);
+
+try {
+    console.log('inside done');
+    // console.log(student);
+    console.log(district);
+    let district = 'bBaria';
+    console.log('After error');
+}
+catch (error) {
+    console.log('inside catch');
+    console.log(error);
+}
+finally {
+    console.log('Finally done');
+}
+console.log('last line of the code')

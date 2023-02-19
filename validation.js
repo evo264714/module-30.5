f65f07105fefb7a4f4ede4ce03a30f6da0fe801a
evo264714 <mh264714@gmail.com>
2023-02-19 20:23:00 +0600

    done

---
+++ validation.js
@@ -0,0 +1,13 @@
+const number = '23';
+if(typeof number ==='number'){
+    console.log('value is a number')
+}
+else{
+    console.log('value is not a number');
+}
+
+const numbers =[3, 12, 78, 90];
+const student = {name:'Evo', job: 'student'};
+// console.log(Array.isArray(numbers));
+console.log(Array.isArray(numbers));
+console.log(typeof numbers);
